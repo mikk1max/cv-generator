a4101e49074afb6056f65c8675be90c9ef39b459
MaX <maksim.shepeta@gmail.com>
2025-05-14 01:02:52 +0200

    fix: mobile adaptive for login and signup
    feat: part-visible users functionality

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import User from './User';
+import React, { useState } from "react";
+import User from "./User";
 import styles from "./styles.module.css";
 
 const Users = ({ users }) => {
-    return (
-        <div className={styles.usersContainer} >
-            {users.map((user) => (
-                <User key={user._id} user={user} />
-            ))}
-        </div>
-    );
+  const [visibleCount, setVisibleCount] = useState(5);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 5);
+  };
+
+  return (
+    <div>
+      <div className={styles.usersContainer}>
+        {users.slice(0, visibleCount).map((user) => (
+          <User key={user._id} user={user} />
+        ))}
+      </div>
+      {users.length > visibleCount && (
+        <button onClick={handleLoadMore} className={styles.loadMoreButton}>
+          Load More
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Users;
